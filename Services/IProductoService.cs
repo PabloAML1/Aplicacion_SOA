38a98a9559861591f3300367ae87ccbd7393d5b4
Pablo Montero <monteropablo511@gmail.com>
2025-09-09 15:24:38 -0500

    Creación de modelos y servicio de Productos

---
+++ Services/IProductoService.cs
@@ -0,0 +1,13 @@
+﻿using Aplicacion_SOA.Models;
+
+namespace Aplicacion_SOA.Services
+{
+    public interface IProductoService
+    {
+        Task<IEnumerable<Producto>> GetAllProductos();
+        Task<Producto?> GetProductoById(int id);
+        Task<Producto> CreateProducto(Producto producto);
+        Task<Producto?> UpdateProducto(int id, Producto producto);
+        Task<bool> DeleteProducto(int id);
+    }
+}
